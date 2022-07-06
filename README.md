fdcf1ea616537e27e149f27cbbd19c55d05e9636
CorysPC <corystory2@gmail.com>
2022-07-05 21:03:09 -0600

    comments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# password-generator
+Welcome to my Password Generator.
+
+Here you will find an application that creates a randomized password for you.  
+You can customize your password with the amount of characters you would like, if you want to use lower case letters, uppercase letters, numbers, and special symbols. 
+You will be prompted through each selection and will be given a random password to use. 
+
+![](./images/passwordscreenshot.png)
--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var lowerCaseCheck = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseCheck = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberCheck = "0123456789";
 var specialCheck ="!@#$%^&*";
+var Characters
+
 
 // Write password to the #password input
 function writePassword() {
@@ -15,11 +17,12 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// Adding and assigning all functions
+// Checking the password length 
 function lengthCheck() {
   passwordLength = prompt("How long would you like your password?");
 
@@ -38,70 +41,50 @@ function lengthCheck() {
   return passwordLength;
   
 };
-
+// asking user if they want to use lower case letters
 function lowerCase() {
   lowerCaseCheck = prompt("Do you want to use lowercase letters?");
 
+  if(lowerCase === true) {
+  
+  }
 return lowerCaseCheck;
 };
-
+//  asking user if they want to use upper case letters
 function upperCase() {
   upperCaseCheck = prompt("Do you want to use Uppercase letters?"); 
 
+  if(upperCase === true) {
+  
+  }
 return upperCaseCheck;
 };
-
+// asking user if they want to use numbers
 function numbers() {
   numberCheck = prompt("Do you want to use Numbers?");
 
+  if(numbers === true) {
+  
+  }
 return numberCheck;  
 };
-
+// asking user if they want to use special characters
 function specials() {
   specialCheck = prompt("Do you want to use Special Characters?");
 
+  if (specials === true) {
+  
+  }
 return specialCheck;  
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// randomizes all characters to create the password
+for(var i = 0; i < passwordLength; i ++) {
+  var randomNumber = Math.floor(Math.random() * character.length)
+  var randomChar = characterString[randomIndex];
+  
+};
+// calling this function to create a password
 function generatePassword() {
   lengthCheck();
   lowerCase();
